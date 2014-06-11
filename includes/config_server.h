0f8fb36f14436a259a7ea766f96263ae671ef3d5
Miroslav Mirkovic <zerborus@hotmail.com>
2014-06-11 19:46:05 +0200

    Prototypen zum Fileserver hinzugefügt

--- includes/config_server.h
+++ includes/config_server.h
@@ -1,1 +1,20 @@
+#include <semaphore.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdio.h>
+
 #define BUFFSIZE 10240
+#define TRUE  1
+#define FALSE 0
+
+struct FileInfo{
+  sem_t semaphore;
+  int semaphoreId;
+  int shmid;
+  unsigned long fileSize;
+  char fileName[256];
+  char *content;
+  int sharedMemoryIdContent;
+  struct *prevFile;
+  struct *nextFile;
+};
